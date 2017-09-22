a1312e5ebc28e39114b4475111bc818a16c41e60
Erin Primrose <eprimrose1@gmail.com>
2017-09-21 22:03:55 -0500

    Updated comments and structure

--- BackEnd/infoFromDb.py
+++ BackEnd/infoFromDb.py
@@ -1,26 +1,37 @@
 import MySQLdb
+import mysql.connector
+
+# connect to MySQL database
+con = mysql.connector.connect(user='sw2proj',password='password',host='cs-lab.letu.edu',database='sw2projdb')
+cursor = con.cursor
 
 class InfoFromDatabase:
 
     def __init__(self):
-
+        # don't know if this is necessary, but it'll stay here in case it is
+        pass
 
     def getMsg(self):
         # pull from Db
+        # this is currently filler
+        msg = "Thank you!"
 
         return msg
 
     def getFromAddr(self):
         # pull from Db
+        # first time would be user, second would be RA, third would be RD
 
         return fromAddr
 
     def getToAddr(self):
         # pull from Db
+        # first would be to RA, then RD, then FS
 
         return toAddr
 
     def getSubject(self):
         # pull from Db
+        # hall, room number, description
 
         return subject
