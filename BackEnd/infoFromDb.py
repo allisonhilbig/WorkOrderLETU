9f465a9e14dfff1475e4c16061be12a121bd2bc4
Erin Primrose <eprimrose1@gmail.com>
2017-09-21 21:27:32 -0500

    Unfinished Email Python files

---
+++ BackEnd/infoFromDb.py
@@ -0,0 +1,26 @@
+import MySQLdb
+
+class InfoFromDatabase:
+
+    def __init__(self):
+
+
+    def getMsg(self):
+        # pull from Db
+
+        return msg
+
+    def getFromAddr(self):
+        # pull from Db
+
+        return fromAddr
+
+    def getToAddr(self):
+        # pull from Db
+
+        return toAddr
+
+    def getSubject(self):
+        # pull from Db
+
+        return subject
