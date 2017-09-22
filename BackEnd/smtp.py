e838a1e54cac44401bd929b331af7ead291aa98c
Erin Primrose <eprimrose1@gmail.com>
2017-09-21 21:39:43 -0500

    Edited smtp python file

--- BackEnd/smtp.py
+++ BackEnd/smtp.py
@@ -1,10 +1,10 @@
 from socket import *
-import infoFromDb
+from infoFromDb import InfoFromDatabase
 
 class SMTP:
 
     endmsg = "\r\n.\r\n"
-    info = InfoFromDb()
+    info = InfoFromDatabase()
 
     # Choose a mail server (e.g. Google mail server) and call it mailserver
     mailserver = ("smtp.letu.edu", 25)
@@ -19,32 +19,32 @@ class SMTP:
 
     # Write message
     def writeMsg(self, message):
-        msg = "\r\n" + info.getMsg()
+        msg = "\r\n" + SMTP.info.getMsg()
         return msg
 
     # Send HELO command and print server response.
     def sendHeloCmd(self):
         heloCommand = 'HELO Alice\r\n'
-        clientSocket.send(heloCommand)
-        recv1 = clientSocket.recv(1024)
+        SMTP.clientSocket.send(heloCommand)
+        recv1 = SMTP.clientSocket.recv(1024)
         print recv1
         if recv1[:3] != '250':
             print '250 reply not received from server.'
 
     # Send MAIL FROM command and print server response.
     def mailFromCmd(self, fromAddr):
-        mailFromCmd = 'MAIL FROM: TheBestestPerson@ever.com\r\n'
-        clientSocket.send(mailFromCmd)
-        recv2 = clientSocket.recv(1024)
+        mailFromCmd = 'MAIL FROM: {}\r\n'.format(SMTP.info.getFromAddr())
+        SMTP.clientSocket.send(mailFromCmd)
+        recv2 = SMTP.clientSocket.recv(1024)
         print recv2
         if recv2[:3] != '250':
             print '250 reply not received from server.'
 
     # Send RCPT TO command and print server response.
     def rcptToCmd(self, toAddr):
-        rcptToCmd = 'RCPT TO: 24ponygirl@gmail.com\r\n'
-        clientSocket.send(rcptToCmd)
-        recv3 = clientSocket.recv(1024)
+        rcptToCmd = 'RCPT TO: {}\r\n'.format(SMTP.info.getToAddr())
+        SMTP.clientSocket.send(rcptToCmd)
+        recv3 = SMTP.clientSocket.recv(1024)
         print recv3
         if recv3[:3] != '250':
             print '250 reply not received from server.'
@@ -52,20 +52,20 @@ class SMTP:
     # Send DATA command and print server response.
     def sendDataCmd(self):
         dataCmd = 'DATA\r\n'
-        clientSocket.send(dataCmd)
-        recv4 = clientSocket.recv(1024)
+        SMTP.clientSocket.send(dataCmd)
+        recv4 = SMTP.clientSocket.recv(1024)
         print recv4
         if recv4[:3] != '250':
             print '250 reply not received from server.'
 
     # add subject
     def addSubjectCmd(self, subject):
-        subjectCommand = "SUBJECT: hi \r\n\n"
+        subjectCommand = "SUBJECT: {}\r\n\n".format(SMTP.info.getSubject())
         print subjectCommand
-        clientSocket.send(subjectCommand)
+        SMTP.clientSocket.send(subjectCommand)
 
     def run(self, message, fromAddr, toAddr, subject):
-        self.writeMsg(message)
+        # self.writeMsg(message)
         self.sendHeloCmd()
         self.mailFromCmd(fromAddr)
         self.rcptToCmd(toAddr)
@@ -73,15 +73,18 @@ class SMTP:
         self.addSubjectCmd(subject)
 
         # Send message data.
-        clientSocket.send(msg)
+        SMTP.clientSocket.send(self.writeMsg(message))
 
         # Message ends with a single period.
-        clientSocket.send(endmsg)
+        SMTP.clientSocket.send(SMTP.endmsg)
 
         # Send QUIT command and get server response.
         quitCmd = 'QUIT\r\n'
-        clientSocket.send(quitCmd)
-        recv5 = clientSocket.recv(1024)
+        SMTP.clientSocket.send(quitCmd)
+        recv5 = SMTP.clientSocket.recv(1024)
         print recv5
         if recv5[:3] != '250':
             print '250 reply not received from server.'
+
+if __name__ == "__main__":
+    SMTP.run(SMTP.info.getMsg(), SMTP.info.getFromAddr(), SMTP.info.getToAddr(), SMTP.info.getSubject())
