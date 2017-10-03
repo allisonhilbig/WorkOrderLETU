4a9454f0d9cb5a20198d95f207be7178d242ce83
Erin Primrose <eprimrose1@gmail.com>
2017-10-02 20:27:08 -0500

    Added rest of queries

--- BackEnd/infoFromDb.py
+++ BackEnd/infoFromDb.py
@@ -11,26 +11,29 @@ class InfoFromDatabase:
         # don't know if this is necessary, but it'll stay here in case it is
         pass
 
-    def getMsg(self, idWorkOrder):
-        # pull from Db
-        # this is currently filler
-        msg = cur.execute("""SELECT Description FROM 'Work Order' WHERE idWorkOrder = '{}'""").format(idWorkOrder)
-        return msg
-
-    def getFromAddr(self):
-        # pull from Db
-        # first time would be user, second would be RA, third would be RD
-
-        return fromAddr
-
-    def getToAddr(self):
-        # pull from Db
-        # first would be to RA, then RD, then FS
-
-        return toAddr
-
-    def getSubject(self):
+    def getSubject(self, idPreference, idWorkOrder):
         # pull from Db
         # hall, room number, description
-
+        subject1 = cur.execute("""SELECT resHall FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference)
+        subject2 = cur.execute("""SELECT roomNumber FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference)
+        subject3 = cur.execute("""SELECT Description FROM 'Work Order' WHERE idWorkOrder = '{}'""").format(idWorkOrder)
+        subject = subject1 + subject2 + subject3
         return subject
+
+    def getFromAddr(self, idPreference):
+        # pull from Db
+        # first time would be user, second would be RA, third would be RD
+        fromAddr = cur.execute("""SELECT userEmail FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference)
+        return fromAddr
+
+    def getToAddr(self, emailForSend, idPreference):
+        # pull from Db
+        # first would be to RA, then RD, then FS
+        toAddr = cur.execute("""SELECT {} FROM 'Preference' WHERE idPreference = '{}'""").format(emailForSend, idPreference)
+        return toAddr
+
+    def getMsg(self, idWorkOrder):
+        # pull from Db
+        # random message?
+        msg = "Thank you!"
+        return msg
