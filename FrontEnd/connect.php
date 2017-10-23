9bdd2c300ced19dc475ccfa7be53dfcac1038297
Unknown <ann_ejones@yahoo.com>
2017-10-23 01:38:44 -0500

    Please test the new file...my laptop wasn't prepared :/

---
+++ FrontEnd/connect.php
@@ -0,0 +1,27 @@
+<?php
+//Step1
+ $db = mysqli_connect('localhost','sw2proj','password','sw2projdb')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+
+<?php
+//Step2
+$query = "SELECT * FROM Preference";
+mysqli_query($db, $query) or die('Error querying database.');
+
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_array($result)) {
+ echo $row['name'] . ' ' . $row['userEmail'] . ': ' . $row['raEMail'] . ' ' . $row['resHall'] .'<br />';
+}
+?>
+
+</body>
+</html>
