4ff9ae84aa12de4793fdd82583561b97cfca6d45
Unknown <ann_ejones@yahoo.com>
2017-11-06 22:43:01 -0600

    Before AJAX attempt; kind of got queries down

--- public_html/connect.php
+++ public_html/connect.php
@@ -1,26 +1,27 @@
 <?php
 //Step1
- $db = mysqli_connect('localhost','sw2proj','password','sw2projdb')
+ $mysqli = mysqli_connect('localhost','sw2proj','password','sw2projdb')
  or die('Error connecting to MySQL server.');
 ?>
 
 <html>
  <head>
- </head>
+ <  /head>
  <body>
  <h1>PHP connect to MySQL</h1>
 
 <?php
 //Step2
-$query = "SELECT * FROM Preference";
-mysqli_query($db, $query) or die('Error querying database.');
+$query = "SELECT userEmail FROM Preference WHERE name = 'annjones'";
+$result = $mysqli->query($query);
+$row = $result->fetch_array(MYSQLI_NUM);
+printf ("%s (%s)\n", $row[0]);
 
-$result = mysqli_query($db, $query);
 $row = mysqli_fetch_array($result);
 
-while ($row = mysqli_fetch_array($result)) {
- echo $row['name'];
-}
+$str = implode(" ",$row);
+echo $str;
+
 ?>
 
 </body>
