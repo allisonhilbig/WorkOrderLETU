3ccd7c6267299bbdbdac848b19b2b168c484d31a
Erin Primrose <eprimrose1@gmail.com>
2017-11-15 11:00:52 -0600

    Added BackEnd to public_html
    includes smtp.py and infoFromDb.py

---
+++ public_html/BackEnd/infoFromDb.py
@@ -0,0 +1,39 @@
+import MySQLdb
+import mysql.connector
+
+# connect to MySQL database
+con = mysql.connector.connect(user='sw2proj',password='password',host='localhost',database='sw2projdb')
+cur = con.cursor()
+
+class InfoFromDatabase:
+
+    def __init__(self):
+        # don't know if this is necessary, but it'll stay here in case it is
+        pass
+
+    def getSubject(self, idPreference, idWorkOrder):
+        # pull from Db
+        # hall, room number, description
+        subjectHall = str(cur.execute("""SELECT resHall FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference))
+        subjectRN = str(cur.execute("""SELECT roomNumber FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference))
+        subjectDesc = str(cur.execute("""SELECT Description FROM 'Work Order' WHERE idWorkOrder = '{}'""").format(idWorkOrder))
+        subject = subjectHall + " " + subjectRN + " " + subjectDesc
+        return subject
+
+    def getFromAddr(self, idPreference):
+        # pull from Db
+        # first time would be user, second would be RA, third would be RD
+        fromAddr = str(cur.execute("""SELECT userEmail FROM 'Preference' WHERE idPreference = '{}'""").format(idPreference))
+        return fromAddr
+
+    def getToAddr(self, emailForSend, idPreference):
+        # pull from Db
+        # first would be to RA, then RD, then FS
+        toAddr = str(cur.execute("""SELECT {} FROM 'Preference' WHERE idPreference = '{}'""").format(emailForSend, idPreference))
+        return toAddr
+
+    def getMsg(self, idWorkOrder):
+        # pull from Db
+        # random message?
+        msg = "Thank you!"
+        return msg
---
+++ public_html/BackEnd/smtp.py
@@ -0,0 +1,105 @@
+from socket import *
+from infoFromDb import InfoFromDatabase
+
+class SMTP:
+
+    endmsg = "\r\n.\r\n"
+    info = InfoFromDatabase()
+
+    # Choose a mail server (e.g. Google mail server) and call it mailserver
+    mailserver = ("smtp.letu.edu", 25)
+    #  Create socket called clientSocket and establish a TCP connection with mailserver
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(mailserver)
+
+    recv = clientSocket.recv(1024)
+
+    # Write message
+    def writeMsg(self, message):
+        msg = "\r\n" + SMTP.info.getMsg()
+        return msg
+
+    # Send HELO command and print server response.
+    def sendHeloCmd(self):
+        heloCommand = 'HELO Alice\r\n'
+        SMTP.clientSocket.send(heloCommand)
+        recv1 = SMTP.clientSocket.recv(1024)
+        print recv1
+        if recv1[:3] != '250':
+            print '250 reply not received from server.'
+
+    # Send MAIL FROM command and print server response.
+    def mailFromCmd(self, fromAddr):
+        mailFromCmd = 'MAIL FROM: {}\r\n'.format(SMTP.info.getFromAddr())
+        SMTP.clientSocket.send(mailFromCmd)
+        recv2 = SMTP.clientSocket.recv(1024)
+        print recv2
+        if recv2[:3] != '250':
+            print '250 reply not received from server.'
+
+    # Send RCPT TO command and print server response.
+    def rcptToCmd(self, toAddr):
+        rcptToCmd = 'RCPT TO: {}\r\n'.format(SMTP.info.getToAddr())
+        SMTP.clientSocket.send(rcptToCmd)
+        recv3 = SMTP.clientSocket.recv(1024)
+        print recv3
+        if recv3[:3] != '250':
+            print '250 reply not received from server.'
+
+    # Send DATA command and print server response.
+    # 250 means email address exists on that server
+    def sendDataCmd(self):
+        dataCmd = 'DATA\r\n'
+        SMTP.clientSocket.send(dataCmd)
+        recv4 = SMTP.clientSocket.recv(1024)
+        print recv4
+        if recv4[:3] != '250':
+            print '250 reply not received from server.'
+
+    def emailVerify(self):
+        trueEmail = False
+        dataCmd = 'DATA\r\n'
+        SMTP.clientSocket.send(dataCmd)
+        isEmailReal = SMTP.clientSocket.recv(1024)[:3]
+        if(isEmailReal == 250):
+            trueEmail = True
+            print('Good Email')
+        else:
+            trueEmail = False
+            print('Bad Email')
+        return trueEmail
+
+    # add subject
+    def addSubjectCmd(self, subject):
+        subjectCommand = "SUBJECT: {}\r\n\n".format(SMTP.info.getSubject())
+        print subjectCommand
+        SMTP.clientSocket.send(subjectCommand)
+
+    def run(self, message, fromAddr, toAddr, subject):
+        # self.writeMsg(message)
+        self.sendHeloCmd()
+        self.mailFromCmd(fromAddr)
+        self.rcptToCmd(toAddr)
+        self.sendDataCmd()
+        # self.emailVerify()                    # don't know if this is necessary, looks like something in mailFromCmd()
+        self.addSubjectCmd(subject)
+
+        # Send message data.
+        SMTP.clientSocket.send(self.writeMsg(message))
+
+        # Message ends with a single period.
+        SMTP.clientSocket.send(SMTP.endmsg)
+
+        # Send QUIT command and get server response.
+        quitCmd = 'QUIT\r\n'
+        SMTP.clientSocket.send(quitCmd)
+        recv5 = SMTP.clientSocket.recv(1024)
+        print recv5
+        if recv5[:3] != '250':
+            print '250 reply not received from server.'
+
+if __name__ == "__main__":
+    print recv
+    if recv[:3] != '220':
+        print '220 reply not received from server.'
+    SMTP.run(SMTP.info.getMsg(), SMTP.info.getFromAddr(), SMTP.info.getToAddr(), SMTP.info.getSubject())
